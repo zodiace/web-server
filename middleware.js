66babad14cece491c5f9ef2cec8fa61b6567772c
Matt Lewis <matthew@matix.me>
2016-01-01 18:23:38 -0400

    Bug fixed with middleware

--- middleware.js
+++ middleware.js
@@ -1,9 +1,12 @@
-module.exports = {
+var middleware = {
 	requireAuthentication: function (req, res, next) {
 		console.log('Private route hit!!')
 		next();
 	},
 	logger: function (req, res, next) {
 		console.log('Request:' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
 	}
 }
+
+module.exports = middleware;
