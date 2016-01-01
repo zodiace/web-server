b27e85976cbc8f52d3ac118e3e5ad963bdafa52e
Matt Lewis <matthew@matix.me>
2016-01-01 18:02:30 -0400

    Update port for Heroku

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var middleware = require('./middleware');
 var app = express();
-var PORT = 3000;
+var PORT = process.evn.PORT || 3000;
 
 // app.get('/', function (req, res) {
 // 	res.send('Hello World!');
 // })
 
+app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us');
 });
-
-app.use(middleware.logger);
 
 app.use(express.static(__dirname + '/public'));
 
